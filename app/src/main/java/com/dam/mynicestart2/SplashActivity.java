66aadf1841e3b8f2735b8523d1af9064d2f2dcb0
smigol222 <santiago.torres27@outlook.com>
2020-11-20 12:17:31 +0100

    Second commit

--- app/src/main/java/com/dam/mynicestart2/SplashActivity.java
+++ app/src/main/java/com/dam/mynicestart2/SplashActivity.java
@@ -5,13 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private TextView subtitulo;
+    private ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        // Implements and starts animation
+
+        logo = (ImageView) findViewById(R.id.logo);
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        logo.startAnimation(myanim);
+
+        subtitulo = (TextView) findViewById(R.id.subtitulo);
+        Animation myanim2 = AnimationUtils.loadAnimation(this, R.anim.fade_in2);
+        subtitulo.startAnimation(myanim2);
 
         openApp(true);
     }
@@ -25,6 +42,6 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        }, 3000);
+        }, 4000);
     }
 }
